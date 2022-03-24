9039973ea74a9f08ac0a0123ad01e4a1a88d6f3d
Vladimir Khodygo <vladimir.khodygo@glasgow.ac.uk>
2022-03-24 18:36:22 +0000

    UPD, DOC: document sum, replace KBK with KBN to stay IEEE compliant

--- src/main/java/jamjam/Sum.java
+++ src/main/java/jamjam/Sum.java
@@ -1,29 +1,39 @@
 package jamjam;
 
 import lombok.NonNull;
+import lombok.val;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
 
 import static java.lang.StrictMath.abs;
+import static java.lang.StrictMath.signum;
 
 public class Sum {
-    public static double KBKSum(double @NonNull [] input){
-        var sum = 0.0;
-        var cs = 0.0;
-        var ccs = 0.0;
-        double c, cc;
-
-        for (var v : input) {
-            double t = sum + v;
-
-            c = abs(sum) >= abs(v) ? (sum - t) + v : (v - t) + sum;
-
-            sum = t;
-            t = cs + c;
-
-            cc = abs(cs) >= abs(c) ? (cs - t) + c : (c - t) + cs;
-
-            cs = t;
-            ccs = ccs + cc;
+    /**
+     * @param x An array of doubles.
+     * @return sum, -Inf, Inf, or NaN.
+     * @implSpec Uses Kahan–Babuška-Klein scheme (scalar), potentially the slowest one available.
+     * @implNote Returns Inf or -Inf in the case of overflow, NaN if the original data contains one, NaN if there is an
+     *           undefined operation such as Infinity - Infinity as per Java specification.
+     * @throws NullPointerException When input is {@code null}.
+     * @throws ArithmeticException When input contains 1 element only.
+     * @see <a href="https://doi.org/10.1007/s00607-005-0139-x">A Generalized Kahan-Babuška-Summation-Algorithm</a>
+     */
+    public static double sum(double @NonNull ... x){
+        System.out.println();
+        if (x.length < 2)
+            throw new ArithmeticException("The size of the array has to be at least 2.");
+        else{
+            var s = x[0];
+            var c = 0.;
+            for (int i = 1; i < x.length; i++){
+                var t = s + x[i];
+                c -= abs(s) >= abs(x[i]) ? ((s - t) + x[i]) : ((x[i] - t) + s);
+                s = t;
+            }
+            return s - c;
         }
-        return sum + cs + ccs;
     }
 }
