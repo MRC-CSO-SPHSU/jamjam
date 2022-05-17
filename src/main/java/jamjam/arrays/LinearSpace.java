d65b72ec19f17501331dd50a21430127e26a327b
Vladimir Khodygo <vladimir.khodygo@glasgow.ac.uk>
2022-05-17 20:49:57 +0100

    UPD, DOC, TEST: add linear space generator, corresponding docs & tests

---
+++ src/main/java/jamjam/arrays/LinearSpace.java
@@ -0,0 +1,61 @@
+package jamjam.arrays;
+
+import org.jetbrains.annotations.Nullable;
+
+import static java.lang.StrictMath.fma;
+
+public class LinearSpace {
+    /**
+     * Creates {@code totalNumber} sub-intervals of the same length from {@code start} to {@code stop}.
+     * @param start Starting (leftmost) point, not included when {@code totalNumber < 1}.
+     * @param stop Stopping (rightmost) point, excluded when {@code endpointIncluded} is {@code false}, included
+     *            otherwise.
+     * @param totalNumber Total number of intervals.
+     * @param endpointIncluded A flag to include the stop point.
+     * @return An array of values, an empty array, null.
+     */
+    public static double @Nullable [] linspace(double start, double stop, int totalNumber, boolean endpointIncluded) {
+        if (totalNumber < 0)
+            return null;
+        if (totalNumber == 0)
+            return new double[]{};
+        if (totalNumber == 1)
+            return new double[]{start};
+
+        double[] values = new double[totalNumber];
+
+        double stepSize = (stop - start) / (endpointIncluded ? totalNumber - 1 : totalNumber);
+
+        for (var i = 0; i < totalNumber; i++)
+            values[i] = fma(i, stepSize, start);
+
+        return values;
+    }
+
+    /**
+     * {@code num} defaults to {@link 50}.
+     *
+     * @see LinearSpace#linspace(double, double, int, boolean)
+     */
+    public static double @Nullable [] linspace(double start, double stop, boolean endpoint) {
+        return linspace(start, stop, 50, endpoint);
+    }
+
+    /**
+     * {@code endpoint} defaults to {@link true}.
+     *
+     * @see LinearSpace#linspace(double, double, int, boolean)
+     */
+    public static double @Nullable [] linspace(double start, double stop, int num) {
+        return linspace(start, stop, num, true);
+    }
+
+    /**
+     * {@code endpoint} defaults to {@link true}, {@code num} defaults to {@link 50}.
+     *
+     * @see LinearSpace#linspace(double, double, int, boolean)
+     */
+    public static double @Nullable [] linspace(double start, double stop) {
+        return linspace(start, stop, 50, true);
+    }
+}
---
+++ src/test/java/jamjam/arrays/LinearSpaceTest.java
@@ -0,0 +1,77 @@
+package jamjam.arrays;
+
+import lombok.val;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinearSpaceTest {
+
+    @Test @DisplayName("General linear space generator.") void linspace() {
+        double[] positiveStop = new double[]{0., 0.5, 1., 1.5, 2., 2.5, 3., 3.5, 4., 4.5, 5., 5.5, 6., 6.5, 7., 7.5, 8.,
+                8.5, 9., 9.5, 10.};
+        assertArrayEquals(positiveStop, LinearSpace.linspace(0., 10., 21, true),
+                "Fails to generate a positive range of values.");
+
+        double[] negativeStop = new double[]{ 0., -0.5, -1., -1.5, -2., -2.5, -3., -3.5, -4., -4.5, -5., -5.5, -6.,
+                -6.5, -7., -7.5, -8., -8.5, -9., -9.5, -10.};
+        assertArrayEquals(negativeStop, LinearSpace.linspace(0., -10., 21, true),
+                "Fails to generate a negative range of values.");
+
+        val v1 = LinearSpace.linspace(0., 10., 100, true);
+        val v2 = LinearSpace.linspace(0., 10., 50, true);
+        assert v1 != null;
+        assert v2 != null;
+        assertEquals(v1[99], v2[49]);
+
+        assertNull(LinearSpace.linspace(0., 10., -1, true), "Negative dimension check fails.");
+        assertArrayEquals(new double[]{}, LinearSpace.linspace(0., 10., 0, true),
+                "Zero interval input fails to return an empty array.");
+        assertArrayEquals(new double[]{-10.}, LinearSpace.linspace(-10., 10., 1, true),
+                "A single value interval must return the start endpoint value, but it fails.");
+    }
+
+    @Test @DisplayName("Linear space generator with fixed length.") void testLinspace() {
+        double[] scratch;
+
+        scratch = LinearSpace.linspace(0., 10., true);
+        assert scratch != null;
+        assertEquals(50, scratch.length,
+                "Length of the output array diverges from the expected (default) value");
+        assertEquals(scratch[scratch.length - 1], 10.,"Actual endpoint value doesn't meet its expected value.");
+
+        scratch = LinearSpace.linspace(0., 10., false);
+        assert scratch != null;
+        assertEquals(scratch[scratch.length - 1], 9.8,"Actual endpoint value doesn't meet its expected value.");
+    }
+
+    @Test @DisplayName("Linear space generator with included endpoint.") void testLinspace1() {
+        val scratch = LinearSpace.linspace(0., 10., 50);
+        assert scratch != null;
+        assertEquals(50, scratch.length,
+                "Length of the output array diverges from the expected (default) value");
+        assertEquals(scratch[scratch.length - 1], 10., "Actual endpoint value doesn't meet its expected value.");
+
+        val range1 = new int[]{120, -100};
+        val range2 = new long[]{120, -100};
+        val range3 = new byte[]{120, -100};
+        val range4 = new short[]{120, -100};
+        assertArrayEquals(LinearSpace.linspace(range1[0], range1[1], 5), LinearSpace.linspace(120., -100., 5),
+                "Cast from int to double fails");
+        assertArrayEquals(LinearSpace.linspace(range2[0], range2[1], 5), LinearSpace.linspace(120., -100., 5),
+                "Cast from long to double fails");
+        assertArrayEquals(LinearSpace.linspace(range3[0], range3[1], 5), LinearSpace.linspace(120., -100., 5),
+                "Cast from byte to double fails");
+        assertArrayEquals(LinearSpace.linspace(range4[0], range4[1], 5), LinearSpace.linspace(120., -100., 5),
+                "Cast from short to double fails");
+    }
+
+    @Test @DisplayName("Linear space generator with included endpoint and fixed length.") void testLinspace2() {
+        val stop = 10.;
+        val v = LinearSpace.linspace(0., stop);
+        assert v != null;
+        assertEquals(50, v.length, "Length of the output array diverges from the expected (default) value");
+        assertEquals(stop, v[v.length - 1], "Last value doesn't match the stop endpoint.");
+    }
+}
