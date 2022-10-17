60bae0bb85c438b2a98b05447ba59f88d89b3dea
Vladimir Khodygo <vladimir.khodygo@glasgow.ac.uk>
2022-10-17 18:06:51 +0100

    TD: statistical distance tests

--- src/test/java/jamjam/probability/StatisticalDistanceTest.java
+++ src/test/java/jamjam/probability/StatisticalDistanceTest.java
@@ -1,14 +1,15 @@
 package jamjam.probability;
 
+import lombok.val;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StatisticalDistanceTest {
 
     @Test
     void kullbackLeiblerDivergence() {
+        val minValue = Math.ulp(1.0);
         assertAll("Method throws a NullPointerException when any parameter is null.",
             () -> assertThrows(NullPointerException.class,
                 () -> StatisticalDistance.KullbackLeiblerDivergence(null, new double[]{0, 1}),
@@ -28,27 +29,27 @@ class StatisticalDistanceTest {
 
         assertAll("All distributions must add up to 1 exactly.",
             () -> assertThrows(IllegalArgumentException.class,
-                () -> StatisticalDistance.KullbackLeiblerDivergence(new double[]{1, Math.ulp(1.0)}, new double[]{1, 0}),
+                () -> StatisticalDistance.KullbackLeiblerDivergence(new double[]{1, minValue}, new double[]{1, 0}),
                 "Distributions with accumulated errors shall not pass, but they do."),
             () -> assertThrows(IllegalArgumentException.class,
-                () -> StatisticalDistance.KullbackLeiblerDivergence(new double[]{1, 0}, new double[]{1, Math.ulp(1.0)}),
+                () -> StatisticalDistance.KullbackLeiblerDivergence(new double[]{1, 0}, new double[]{1, minValue}),
                 "Distributions with accumulated errors shall not pass, but they do."));
 
         assertAll("Method throws a NullPointerException when any of the passed values is out of [0, 1]",
             () -> assertThrows(IllegalArgumentException.class,
                 () -> StatisticalDistance.KullbackLeiblerDivergence(new double[]{1, 0},
-                    new double[]{0 - Math.ulp(1.0), 1 + Math.ulp(1.0)}),
+                    new double[]{0 - minValue, 1 + minValue}),
                 "Check of the actual distribution fails to fail."),
             () -> assertThrows(IllegalArgumentException.class,
-                () -> StatisticalDistance.KullbackLeiblerDivergence(new double[]{0 - Math.ulp(1.0),
-                    1 + Math.ulp(1.0)}, new double[]{1, 0}),
+                () -> StatisticalDistance.KullbackLeiblerDivergence(new double[]{0 - minValue,
+                    1 + minValue}, new double[]{1, 0}),
                 "Check of the reference distribution fails to fail."));
 
         assertTrue(Double.isFinite(StatisticalDistance.KullbackLeiblerDivergence(new double[]{0, 1},
             new double[]{1, 0})), "Trimming should result in limited values, not Infinity");
 
-        assertEquals(StatisticalDistance.KullbackLeiblerDivergence(new double[]{0.5, 0.5}, new double[]{0.5, 0.5}),
-            Double.MIN_NORMAL,
+        assertEquals(Double.MIN_NORMAL,
+            StatisticalDistance.KullbackLeiblerDivergence(new double[]{0.5, 0.5}, new double[]{0.5, 0.5}),
             "Equal distributions must result in 0 (actual) or Double.MIN_NORMAL (trimmed), but they don't");
     }
 
